e90fc9fa0f0b98c6bbf28b746127651657d574ac
Charly <larocca.charly@gmail.com>
2025-02-13 11:20:14 -0500

    download solution

--- env.sh
+++ env.sh
@@ -1,5 +1,8 @@
 alias createjobs="uv run examples/create_jobs.py"
 alias queuejobs="uv run examples/queue_jobs.py"
+alias downloadsolution="uv run examples/download_solution.py"
+
+
 alias build="uv build"
 alias publish="uv publish"
 alias opt="uv run opt" 
