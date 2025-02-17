77c9063d3f70f1d75eb0f573ff9ce9ef18333a46
Charly <larocca.charly@gmail.com>
2025-02-17 09:36:34 -0500

    move send_stream_request

--- src/mathoptdev/send_request.py
+++ src/mathoptdev/send_request.py
@@ -25,21 +25,3 @@ def send_request(body: dict) -> dict:
     assert type(response_dict) == dict
     return response_dict
 
-def send_stream_request(body: dict) -> list:
-    """Send a streaming request to the API and return collected responses"""
-    response = requests.post(
-        get_api_endpoint(),
-        headers=get_auth_headers(),
-        json=body,
-        stream=True
-    )
-    handle_raise_for_status(response)
-    
-    collected_responses = []
-    for line in response.iter_lines():
-        if line:
-            decoded_line = line.decode('utf-8')  
-            print(decoded_line)
-            collected_responses.append(decoded_line)
-    
-    return collected_responses
