d47c043891c0ca9d4be5edf0945c9903cc7f22fa
laroccacharly <larocca.charly@gmail.com>
2025-03-03 14:21:03 -0500

    Only queue the first job

--- examples/queue_jobs.py
+++ examples/queue_jobs.py
@@ -7,7 +7,9 @@ jobs = jobs_data['jobs']
 pending_jobs = [job for job in jobs if job['status'] == 'PENDING']
 
 print(f"Found {len(pending_jobs)} pending jobs")
+# Only queue the first job 
+job_ids = [job['id'] for job in pending_jobs[:1]]
 
-request = QueueJobsRequest(job_ids=[job['id'] for job in pending_jobs])
+request = QueueJobsRequest(job_ids=job_ids)
 response = opt.queue_jobs(request)
 opt.pretty_log(response)
